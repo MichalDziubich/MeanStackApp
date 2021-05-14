ddde0177d12123fdb07d12b6830e1ce19deb33cb
Michal Dziubich <michal.dziubich@gmail.com>
2021-05-14 12:00:57 +0200

    updating backend and front

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
@@ -44,6 +44,17 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    res.status(200).json({ message: "Update successful" })
+  })
+})
+
 app.get("/api/posts", (req, res, next) => {
   Post.find().then(documents => {
     res.status(200).json({
@@ -52,6 +63,16 @@ app.get("/api/posts", (req, res, next) => {
     });
   });
 });
+
+app.get("api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ mesage: 'Post not found!' })
+    }
+  })
+})
 
 app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(result => {
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,28 +1,59 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from '../post.service';
+import { Post } from "../post.model"
+
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
-export class PostCreateComponent {
-  enteredTitle = '';
-  enteredContent = '';
+export class PostCreateComponent implements OnInit {
+  enteredTitle = "";
+  enteredContent = "";
+  post: Post;
+  private mode = "create";
+  private postId: string;
 
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) { }
 
-  constructor(public postsService: PostsService) { }
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = { id: postData._id, title: postData.title, content: postData.content }
+        });
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
+  }
 
-
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
+    if (this.mode === "create") {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
+    }
 
 
-    this.postsService.addPost(form.value.title, form.value.content);
+
     form.resetForm();
 
 
